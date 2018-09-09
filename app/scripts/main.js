4227b7ad10a854af84c0e95ba12b6b298f64f397
fei.wang <fei.wang@loncus.com>
2018-09-09 22:45:35 +0800

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+const client = new ClientJS();
+$(function() {
+  const bodyEle = document.getElementsByTagName('body')[0];
+  const bodyHeight = bodyEle.clientHeight;
+  const bodyWith = bodyEle.clientWidth;
+  const clientObj = {
+    agent: client.getUserAgent(),
+    os: client.getOS(),
+    screen: client.getScreenPrint(),
+    body: `Width:${bodyWith},Height:${bodyHeight}`,
+    timeZone: client.getTimeZone(),
+    language: client.getLanguage(),
+    fonts: client.getFonts()
+  };
+  const clientStr = Object.keys(clientObj).map(
+    key => `<li><span class="title">${key}</span><span class="txt">${clientObj[key]}</span></li>`
+  );
+  $('#slide0 ul').html(clientStr);
+});
